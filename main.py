684d4eed95e195968e0bbcd00c74ac8ef2aa4506
s33zhong <shanhezhong@gmail.com>
2023-05-12 20:00:43 -0400

    Update node_graph implemented generate_square_grid and visualize_graph

--- main.py
+++ main.py
@@ -1,28 +1,33 @@
 import node_graph, node_list, pathfinding_algorithm
+import networkx as nx
+import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
     graph = node_graph.WeightedMultiGraph()
-    node_1 = node_graph.Node(name='node 1')
-    node_2 = node_graph.Node(name='node 2')
-    node_3 = node_graph.Node(name='node 3')
-    node_4 = node_graph.Node(name='node 4')
-    node_5 = node_graph.Node(name='node 5')
-    node_6 = node_graph.Node(name='node 6')
-    node_7 = node_graph.Node(name='node 7')
+    graph.generate_square_grid(6, 5)
 
-    graph.append_node(node_1)
-    graph.append_node(node_2, adjacent_nodes=[node_1], edge_costs=[1])
-    graph.append_node(node_3, adjacent_nodes=[node_1], edge_costs=[1])
-    graph.append_node(node_4, adjacent_nodes=[node_3], edge_costs=[6])
-    graph.append_node(node_5, adjacent_nodes=[node_4], edge_costs=[4])
-    graph.append_node(node_6, adjacent_nodes=[node_3], edge_costs=[2])
-    graph.append_node(node_7, adjacent_nodes=[node_2], edge_costs=[2])
+    # graph = node_graph.WeightedMultiGraph()
+    # node_1 = node_graph.Node(name='node 1')
+    # node_2 = node_graph.Node(name='node 2')
+    # node_3 = node_graph.Node(name='node 3')
+    # node_4 = node_graph.Node(name='node 4')
+    # node_5 = node_graph.Node(name='node 5')
+    # node_6 = node_graph.Node(name='node 6')
+    # node_7 = node_graph.Node(name='node 7')
+    #
+    # graph.append_node(node_1)
+    # graph.append_node(node_2, adjacent_nodes=[node_1], edge_costs=[1])
+    # graph.append_node(node_3, adjacent_nodes=[node_1], edge_costs=[1])
+    # graph.append_node(node_4, adjacent_nodes=[node_3], edge_costs=[6])
+    # graph.append_node(node_5, adjacent_nodes=[node_4], edge_costs=[4])
+    # graph.append_node(node_6, adjacent_nodes=[node_3], edge_costs=[2])
+    # graph.append_node(node_7, adjacent_nodes=[node_2], edge_costs=[2])
 
     bfs_alg = pathfinding_algorithm.BreathFirstSearch()
-    print(bfs_alg.find_path(graph, node_1, node_7, "forward"))
+    print(bfs_alg.find_path(graph, graph.nodes['node 1'], graph.nodes['node 22'], "forward"))
 
     dfs_alg = pathfinding_algorithm.BreathFirstSearch()
-    print(dfs_alg.find_path(graph, node_1, node_7, "forward"))
+    print(dfs_alg.find_path(graph, graph.nodes['node 1'], graph.nodes['node 22'], "forward"))
 
-
+    graph.visualize_graph()
 #%%
--- node_graph.py
+++ node_graph.py
@@ -1,3 +1,6 @@
+import networkx as nx
+import numpy as np
+
 class Node:
     def __init__(self, name: str):
         self.name = name
@@ -8,6 +11,83 @@ class Node:
         self.f_cost = None
         self.g_cost = None
         self.h_cost = None
+        self.optimal_cost = None
+
+        # Need this for calculating the heuristic cost
+        self.x = None
+        self.y = None
+
+
+class WeightedGraph:
+    def __init__(self, graph_type="undirected"):
+        # The collection of nodes' names and its corresponding instances
+        self.nodes = {}
+        self.graph_type = graph_type
+
+    def generate_random_graph(self, size: int):
+        pass
+
+    def generate_square_grid(self, width: int, height: int):
+        for i in range(height):
+            for j in range(width):
+                generated_node = Node(name="node {}".format(i * j))
+
+                # need this for calculating the heuristic cost
+                generated_node.x = i
+                generated_node.y = j
+                adjacency_info = []
+                if i != 0:  # if not the first row
+                    adjacency_info.append({'node name': "node {}".format(i * (j - 1)), 'edge cost': 1})
+                if i != height - 1:  # if not the last row
+                    adjacency_info.append({'node name': "node {}".format(i * (j + 1)), 'edge cost': 1})
+                if j != 0:  # if not the first column
+                    adjacency_info.append({'node name': "node {}".format(i * j - 1), 'edge cost': 1})
+                if j != width - 1:  # if not the last column
+                    adjacency_info.append({'node name': "node {}".format(i * j + 1), 'edge cost': 1})
+
+                generated_node.adjacency_info = adjacency_info  # add this node's adjacency_info to it
+                self.nodes[generated_node.name] = generated_node  # add this node to the graph
+    def visualize_graph(self):
+        pass
+
+    def append_node(self, current_node: Node, adjacent_nodes=None, edge_costs=None):
+        """
+        Adds a new node with its adjacent nodes and the corresponding edge costs to the node list
+        :param current_node:                a Node instance
+        :param adjacent_nodes:      a list of Node instances of length n that node is connected to
+        :param edge_costs:          a list of edge costs of length n that corresponding to the adjacent_nodes
+        :return:
+        """
+        # add the new node to the graph
+        if current_node is None:
+            raise ValueError("A Node instance is required to call the append_node function.")
+
+        adjacency_info = []
+        node_edge_count = 0
+        if adjacent_nodes is not None and edge_costs is not None:
+            for adjacent_node, edge_cost in zip(adjacent_nodes, edge_costs):
+                node_edge_count += 1
+                # print("The adjacent node is: {}\n".format(adjacent_node.name),
+                #       "The edge cost is: {}\n".format(edge_cost))
+                if adjacent_node.name == current_node.name:
+                    raise ValueError("We assumed no self-looping nodes as it is not useful in pathfinding.")
+
+                if self.graph_type == "undirected":  # add the edge back if is an undirected graph
+                    # record the adjacent nodes' info to be put into node.adjacency_info
+                    #   note the node instance can be accessed through self.nodes[node.name]
+                    adjacency_info.append({"node name": adjacent_node.name,
+                                           "edge cost": edge_cost})
+
+                    # add this node back to the adjacent node's attribute; the edge index should be the same
+                    self.nodes[adjacent_node.name].adjacency_info.append({"node name": current_node.name,
+                                                                          "edge cost": edge_cost})
+
+        current_node.adjacency_info = adjacency_info  # add this nodes adjacency_info to the node
+        self.edge_counts[current_node.name] = node_edge_count
+        self.nodes[current_node.name] = current_node
+
+    def remove_node(self, node: Node):
+        pass
 
 
 class WeightedMultiGraph:
@@ -29,8 +109,43 @@ class WeightedMultiGraph:
     def generate_random_graph(self, size: int):
         pass
 
+    def generate_square_grid(self, width: int, height: int):
+        for i in range(height):
+            for j in range(width):
+                current_node_count = i * width + j
+                generated_node = Node(name="node {}".format(current_node_count))
+
+                # need this for calculating the heuristic cost
+                generated_node.x = i
+                generated_node.y = j
+                adjacency_info = []
+
+                if i != 0:  # if not the first row
+                    adjacency_info.append({'node name': "node {}".format(current_node_count - width),
+                                           'edge cost': 1})  # the node above
+                if i != height - 1:  # if not the last row
+                    adjacency_info.append({'node name': "node {}".format(current_node_count + width),
+                                           'edge cost': 1})  # the node below
+                if j != 0:  # if not the first column
+                    adjacency_info.append({'node name': "node {}".format(current_node_count - 1),
+                                           'edge cost': 1})  # the node to the left
+                if j != width - 1:  # if not the last column
+                    adjacency_info.append({'node name': "node {}".format(current_node_count + 1),
+                                           'edge cost': 1})  # the node to the right
+
+                generated_node.adjacency_info = adjacency_info  # add this node's adjacency_info to it
+                self.nodes[generated_node.name] = generated_node  # add this node to the graph
+
     def visualize_graph(self):
-        pass
+        nx_graph = nx.Graph()
+
+        for node in self.nodes.values():
+            nx_graph.add_node(node.name)
+            for adjacent_node in node.adjacency_info:
+                nx_graph.add_edge(node.name, adjacent_node['node name'],
+                                  weight=adjacent_node['edge cost'])
+
+        nx.draw_networkx(nx_graph, with_labels=True, font_weight='bold')
 
     def append_node(self, current_node: Node, adjacent_nodes=None, edge_costs=None):
         """
@@ -83,9 +198,5 @@ class WeightedMultiGraph:
         pass
 
 
-class MultiGraph(WeightedMultiGraph):
-    # Just a special case where it has uniform cost, so it is a subclass
-
-    pass
 
 #%%
